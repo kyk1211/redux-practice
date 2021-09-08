e4dbbfbc9f69d5f6885a52d92837c1fe49426b55
곽윤구 <dbsrndi123@naver.com>
2021-09-08 21:53:37 +0900

    chapter2 - 1

--- src/index.js
+++ src/index.js
@@ -1,46 +1,39 @@
 import { createStore } from "redux";
 
-const plus = document.getElementById('plus');
-const minus = document.getElementById('minus');
-const number = document.querySelector('span');
+const form = document.querySelector('form');
+const input = form.querySelector('input');
+const ul = document.querySelector('ul');
 
-number.innerText = 0;
-// state: changeable date (count)
-// reducer: change data, return => data, reducer return state
-// action: communicate with reducer, action need object, need 'type' property
-// store.dispatch: add action to reducer
-// subscribe: have function, listen change in store and run function
+const ADD_TODO = 'ADD_TODO';
+const DELETE_TODO = 'DELETE_TODO';
 
-const ADD = 'ADD';
-const MINUS = 'MINUS';
-
-const countModifier = (count = 0, action) => {
+const reducer = (state = [], action) => {
   switch (action.type) {
-    case ADD:
-      return count + 1;
-    case MINUS:
-      return count - 1;
+    case ADD_TODO:
+      return [...state, { text: action.text, id: Date.now() }];
+    case DELETE_TODO:
+      return [];
     default:
-      return count;
+      return state;
   }
 };
 
-const countStore = createStore(countModifier);
+const store = createStore(reducer);
 
-const onChange = () => {
-  number.innerText = countStore.getState();
+// store.subscribe(() => {
+//   const ToDos = store.getState();
+//   ToDos.map((toDo) => {
+//     let li = document.createElement(li);
+//     li.innerText =toDo.text;
+//     ul.appendChild(li);
+//   });
+// });
+
+const onSubmit = (e) => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = '';
+  store.dispatch({ type: ADD_TODO, text: toDo });
 };
 
-countStore.subscribe(onChange);
-
-const handleAdd = () => {
-  countStore.dispatch({ type: ADD });
-};
-
-const handleMinus = () => {
-  countStore.dispatch({ type: MINUS });
-};
-
-plus.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
-
+form.addEventListener('submit', onSubmit);
